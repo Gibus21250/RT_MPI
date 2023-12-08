3304af164232ef938ad482a2fedb5755d62b6fd5
Nathan J <jacquinet.nathan@gmail.com>
2023-12-08 21:09:44 +0100

    test vector

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include "math/vector.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char const **argv)
 {
-    printf("Hello Word\n");
+    Vector v1 = {1.0, 0, 0};
+
+    printf("%f %f %f\n", v1.x, v1.y, v1.z);
+    normalize(&v1);
+
+    printf("%f %f %f\n", v1.x, v1.y, v1.z);
+
+    Vector v2 = {0, 1, 0};
+
+    Vector v3 = cross(&v1, &v2);
+
+    printf("%f %f %f\n", v3.x, v3.y, v3.z);
     return 0;
 }
+
+/*
+MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("Hello from process %d\n", rank);
+
+    MPI_Finalize();
+*/
