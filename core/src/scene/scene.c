a41ef53587e9c8c0b1fe84882bddc9e9857dee8e
Nathan J <jacquinet.nathan@gmail.com>
2023-12-09 22:46:55 +0100

    comments

--- core/src/scene/scene.c
+++ core/src/scene/scene.c
@@ -29,12 +29,13 @@ Color launchRay(Scene *scene, Ray *ray)
     
     //printf("o: %lf %lf %lf\n", ray->o.x, ray->o.y, ray->o.z);
     //printf("dir: %lf %lf %lf\n\n", ray->v.x, ray->v.y, ray->v.z);
-    Color res = {0, 0, 0};
+    Color res = {0, 0.5, 0.8};
     //Il faut garder une trace de ce qui a été touché, en gardant le plus proche intersecté
     double t = DBL_MAX;
 
     //Array des Spheres
     ModelsArray* sphereArray = &scene->spheres;
+    //Pour chaque sphere
     for(unsigned int i = 0; i < sphereArray->nb; ++i)
     {
         double tTmp = intersectSphere(ray, &((Sphere*) sphereArray->tab)[i]);
