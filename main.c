8893c1c5274eab37ec489ebba338abe1939ef3d3
Nathan J <jacquinet.nathan@gmail.com>
2023-12-09 20:48:56 +0100

    ajout sphere

--- main.c
+++ main.c
@@ -10,22 +10,24 @@
 
 int main(int argc, char const **argv)
 {
-    Screen ecran = {NULL, 512, 512};
+    Screen ecran = {NULL, 1024, 1024};
     Scene scene;
     Camera camera = {{0, 0, 0}, {1, 0, 0}, {0, 1, 0}};
 
     initScreen(&ecran);
     initScene(&scene);
 
-    Sphere test = {{0, 0.5, 0.8}, 0.5, {0, 0, 2}};
+    Sphere test = {{0.4, 0.5, 0.8}, 0.5, {0, 1, 1}};
+    Sphere test2 = {{0.8, 0.1, 0.4}, 1, {0, 0, 3}};
 
     addModel(&scene, &test);
+    addModel(&scene, &test2);
 
     draw(&ecran, &camera, &scene);
     //showResult(&ecran);
     
     savePPMP6(ecran.screen, ecran.l, ecran.L, "premier6.ppm");
-    savePPMP3(ecran.screen, ecran.l, ecran.L, "premier3.ppm");
+    //savePPMP3(ecran.screen, ecran.l, ecran.L, "premier3.ppm");
 
     destroyScreen(&ecran);
     destroyScene(&scene);
