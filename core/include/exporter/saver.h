3f01b3d70ab03c888638ece038a5193d9ff03bea
Nathan J <jacquinet.nathan@gmail.com>
2023-12-09 18:49:19 +0100

    ajout format PPMP3
    Il y a une rotation entre une sauvegarde p3 et p3, à voir le quel est le bon, puis corriger l'autre

--- core/include/exporter/saver.h
+++ core/include/exporter/saver.h
@@ -6,6 +6,6 @@
 /**
  * Sauvegarde dans un fichier name, une image de longueur l, et largeur L
 */
-char save(Color *image, unsigned int l, unsigned int L, char *name);
-
+char savePPMP6(Color *image, unsigned int l, unsigned int L, char *name);
+char savePPMP3(Color *image, unsigned int l, unsigned int L, char *name);
 #endif
--- core/src/exporter/saver.c
+++ core/src/exporter/saver.c
@@ -1,7 +1,8 @@
 #include "exporter/saver.h"
 #include <stdio.h>
 
-char save(Color *image, unsigned int l, unsigned int L, char *name)
+
+char savePPMP6(Color *image, unsigned int l, unsigned int L, char *name)
 {
     FILE *pfile;
 
@@ -9,12 +10,47 @@ char save(Color *image, unsigned int l, unsigned int L, char *name)
 
     if (pfile)
     {
-        fprintf(pfile, "P6\n%u %u\n255\n", L, l);
+        fprintf(pfile, "P6\n%u %u\n255\n", l, L);
 
-        //pour chaque ligne
+        //pour chaque colonne
         for (unsigned int i = 0; i < l; ++i)
         {
-            //Pour chaque colonne
+            //Pour chaque ligne
+            for (unsigned int j = 0; j < L; ++j)
+            {
+                //On récupère en valuer [0;255] la couleur
+                unsigned char r = (unsigned char)(image[i * L + j].r * 255.0f);
+                unsigned char g = (unsigned char)(image[i * L + j].g * 255.0f);
+                unsigned char b = (unsigned char)(image[i * L + j].b * 255.0f);
+
+                //Ecrire en binaire les valeur des couleurs, pour gagner en place!
+                fwrite(&r, sizeof(unsigned char), 1, pfile);
+                fwrite(&g, sizeof(unsigned char), 1, pfile);
+                fwrite(&b, sizeof(unsigned char), 1, pfile);
+            }
+        }
+
+        fclose(pfile);
+        return 1;
+    }
+    else
+        return 0;
+}
+
+char savePPMP3(Color *image, unsigned int l, unsigned int L, char *name)
+{
+    FILE *pfile;
+
+    pfile = fopen(name, "wb");
+
+    if (pfile)
+    {
+        fprintf(pfile, "P3\n%u %u\n255\n", l, L);
+
+        //pour chaque colonne
+        for (unsigned int i = 0; i < l; ++i)
+        {
+            //pour chaque ligne
             for (unsigned int j = 0; j < L; ++j)
             {
                 //On récupère en valuer [0;255] la couleur
@@ -22,10 +58,7 @@ char save(Color *image, unsigned int l, unsigned int L, char *name)
                 unsigned char g = (unsigned char)(image[i + L * j].g * 255.0f);
                 unsigned char b = (unsigned char)(image[i + L * j].b * 255.0f);
 
-                //Ecrire en binaire les valeur des couelurs, pour gagner en place!
-                fwrite(&r, sizeof(unsigned char), 1, pfile);
-                fwrite(&g, sizeof(unsigned char), 1, pfile);
-                fwrite(&b, sizeof(unsigned char), 1, pfile);
+                fprintf(pfile, "%u %u %u ", (unsigned int)r, (unsigned int)g, (unsigned int)b);
             }
 
             fprintf(pfile, "\n");
@@ -36,7 +69,4 @@ char save(Color *image, unsigned int l, unsigned int L, char *name)
     }
     else
         return 0;
-
- 
-    
 }
