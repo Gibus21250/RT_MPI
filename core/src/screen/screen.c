4b599820d980173ca3ed886811c78a1177b9b540
Nathan J <jacquinet.nathan@gmail.com>
2023-12-09 22:46:30 +0100

    ajout vue perspective camera

--- core/src/screen/screen.c
+++ core/src/screen/screen.c
@@ -13,44 +13,78 @@
 
 void draw(Screen* screen, Camera *camera, Scene *scene)
 {
-
     //Calcule du biais (pour lancer le rayon au centre du pixel, au lieu d'un coin)
     double biaisx = (double) 1 / screen->L;
     double biaisy = (double) 1 / screen->l;
 
-    printf("%lf %lf\n", biaisx, biaisy);
+    //printf("%lf %lf\n", biaisx, biaisy);
     //On va lancer un rayon depuis chaque pixel de l'écran
     //Toute les lignes
-    for (unsigned int i = 0; i < screen->L; i++)
+
+    //Si la caméra est orthogonlale/parallele:
+    if(camera->type == PARALLEL)
     {
-        //Toutes les colonnes de chaque ligne
-        for (unsigned int j = 0; j < screen->l; ++j)
+        for (unsigned int i = 0; i < screen->L; i++)
         {
-            // Calcul des coordonnées normalisées du pixel sur l'écran
+            //Toutes les colonnes de chaque ligne
+            for (unsigned int j = 0; j < screen->l; ++j)
+            {
+                // Calcul des coordonnées normalisées du pixel sur l'écran
 
-            double xEcran = 2 * ((double) j / (double) screen->l) - 1;
-            double yEcran = 1 - 2 * ((double) i / (double) screen->L);
+                double xEcran = 2 * ((double) j / (double) screen->l) - 1;
+                double yEcran = 1 - 2 * ((double) i / (double) screen->L);
 
-            Ray ray = {{xEcran + biaisx, yEcran - biaisy, 0}, {0, 0, 1}};
+                Ray ray = {{xEcran + biaisx, yEcran - biaisy, 0}, camera->projection.parallel.direction};
 
-            screen->screen[i * screen->l + j] = launchRay(scene, &ray);
+                screen->screen[i * screen->l + j] = launchRay(scene, &ray);
+            }
         }
     }
-    
+    else //Camera perspective
+    {
+        // Calcul de la base de la caméra
+        Vector view = sub(&camera->projection.perspective.lookAt, &camera->projection.perspective.position);
+        normalize(&view);
+
+        Vector right = cross(&camera->projection.perspective.up, &view);
+        normalize(&right);
+
+        Vector up = cross(&view, &right);
+
+        // Point de perspective (centre de l'écran)
+        Vector perspective_point = add(&camera->projection.perspective.position, &view);
+
+        // Distance entre la caméra et le plan de l'écran
+        double distance_to_screen = 1.0; // Ajustez selon vos besoins
+
+        for (unsigned int i = 0; i < screen->L; i++)
+        {
+            //Toutes les colonnes de chaque ligne
+            for (unsigned int j = 0; j < screen->l; ++j)
+            {
+                // Calcul des coordonnées normalisées du pixel sur l'écran
+                double xEcran = 2 * ((double) j / (double) screen->l) - 1;
+                double yEcran = 1 - 2 * ((double) i / (double) screen->L);
+
+                // Calcul de la direction du rayon en fonction des coordonnées du pixel
+                Vector rx = mul(&right, xEcran); //Composante droite de la direction du rayon
+                Vector uy = mul(&up, yEcran);   //Composante haut de la direction du rayon
+                Vector rxup = add(&rx, &uy);    //Direction du vecteur (depuis l'origine de l'écran)
+
+                Vector vdist = mul(&view, distance_to_screen);
+
+                Vector ray_direction = add(&rxup, &vdist);
+
+                normalize(&ray_direction);
+
+                Ray ray = {perspective_point, ray_direction};
+
+                screen->screen[i * screen->l + j] = launchRay(scene, &ray);
+            }
+        }
+    }
 }
 
-
-
-
-
-
-/*
-
-
-double x_normalized = pixelsx + (2.0 * j - screen->L) / screen->L;
-            double y_normalized = pixelsy + (2.0 * i - screen->l) / screen->l;
-
-*/
 
 void showResult(Screen *screen)
 {
