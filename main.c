2db72fd73957010a847ebe9e6993dceff4f978f4
Nathan J <jacquinet.nathan@gmail.com>
2023-12-10 16:56:22 +0100

    modification jeu de test

--- main.c
+++ main.c
@@ -4,22 +4,27 @@
 #include "screen/screen.h"
 #include "scene/scene.h"
 #include "scene/camera.h"
+#include "scene/light.h"
 
 #include "exporter/color.h"
 #include "exporter/saver.h"
 
 int main(int argc, char const **argv)
 {
-    Screen ecran = {NULL, 4096, 4096};
+    //64 36 -> 16/9
+    Screen ecran = {NULL, 512, 512};
     Scene scene;
+    Color ambiant = {.2, .2, .2};
+    scene.ambiant = ambiant;
 
     Camera perspCam = {
         .type = PERSPECTIVE, 
         .projection = {
             .perspective = {
-                .position = {5, 5, -5},
-                .lookAt = {0, 0, 0},
-                .up = {0, 1, 0}
+                .position = {0, 0, -1},
+                .lookAt = {0, 0, 1},
+                .up = {0, 1, 0},
+                .distance = 1
             }
         }
     };
@@ -34,20 +39,28 @@ int main(int argc, char const **argv)
         }
     };
 
+    PointLight light = {{0, 1, 0}, 1., {1, 1, 1}};
+    PointLight light2 = {{0, 0, 0}, 1.0, {1, 0, 0}};
+    PointLight light3 = {{0, -1, 0}, 1.0, {0, 1, 0}};
+
     initScreen(&ecran);
     initScene(&scene);
 
-    Sphere test = {{0.4, 0.5, 0.8}, 0.5, {0, 0.1, 0.9}}; //bleu
-    Sphere test2 = {{0.8, 0.1, 0.4}, 0.2, {-0.5, 0.5, 1}}; //rose
-    Sphere test3 = {{0.1, 0.1, 0.4}, 0.2, {0.5, 0.5, 1}}; //rose
+    Sphere test = {{1, 1, 1}, 0.5, {0, 0, 1}}; //bleu
+    Sphere test2 = {{1, 1, 1}, 0.2, {-0.5, 0.5, 1}};
+    Sphere test3 = {{1, 1, 1}, 0.2, {0.5, 0.5, 1}};
 
     addModel(&scene, &test);
     addModel(&scene, &test2);
     addModel(&scene, &test3);
 
+    addLight(&scene, &light);
+    addLight(&scene, &light2);
+    addLight(&scene, &light3);
+
     draw(&ecran, &perspCam, &scene);
     //showResult(&ecran);
-    
+    printf("finit\n");
     savePPMP6(ecran.screen, ecran.l, ecran.L, "premier6.ppm");
     //savePPMP3(ecran.screen, ecran.l, ecran.L, "premier3.ppm");
 
