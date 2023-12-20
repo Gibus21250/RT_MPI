be1d0c5a2e9c81940337bb7dd72178497b6e659c
gibus21250 <jacquinet.nathan@gmail.com>
2023-12-20 15:46:02 +0100

    ajout constant MPI_Struct

--- mpi/struct_types/struct_type_all.h
+++ mpi/struct_types/struct_type_all.h
@@ -4,14 +4,14 @@
 #include <mpi.h>
 
 
-    MPI_datatype MPI_COLOR_STRUCT;
-    MPI_datatype MPI_VECTOR_STRUCT;
+    extern MPI_datatype MPI_COLOR_STRUCT;
+    extern MPI_datatype MPI_VECTOR_STRUCT;
 
-    MPI_datatype MPI_CAMERA_STRUCT;
-    MPI_datatype MPI_SCREEN_STRUCT;
-    MPI_datatype MPI_SCENE_STRUCT;
+    extern MPI_datatype MPI_CAMERA_STRUCT;
+    extern MPI_datatype MPI_SCREEN_STRUCT;
+    extern MPI_datatype MPI_SCENE_STRUCT;
 
-    MPI_datatype MPI_MATERIAL_STRUCT;
-    MPI_datatype MPI_SPHERE_STRUCT;
+    extern MPI_datatype MPI_MATERIAL_STRUCT;
+    extern MPI_datatype MPI_SPHERE_STRUCT;
 
 #endif
