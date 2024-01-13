e886b536c30c7044a5de35edd5b820d51c1a45e5
Nathan J <jacquinet.nathan@gmail.com>
2024-01-13 17:11:27 +0100

    séparation intersection de ray

--- core/include/models/ray.h
+++ core/include/models/ray.h
@@ -2,14 +2,11 @@
 #define RAY_H
 
 #include "utils/vector.h"
-#include "sphere.h"
 
 typedef struct Ray {
     Vector o;
     Vector v;
 } Ray;
-
-double intersectSphere(Ray *r, Sphere *sphere);
 
 /**
  * Avance de t dans la direction du rayon
--- core/src/models/ray.c
+++ core/src/models/ray.c
@@ -1,40 +1,4 @@
 #include "models/ray.h"
-#include "utils/vector.h"
-
-#include <math.h>
-
-/**
- * Renvoie le t de l'intersection, -1 sinon
-*/
-double intersectSphere(Ray *r, Sphere *sphere)
-{
-    Vector oc = {
-        r->o.x - sphere->center.x,
-        r->o.y - sphere->center.y,
-        r->o.z - sphere->center.z
-    };
-
-    double a = dot(&r->v, &r->v);
-    double b = 2.0 * dot(&oc, &r->v);
-    double c = dot(&oc, &oc) - sphere->r * sphere->r;
-
-    double delta = b*b - 4*a*c;
-
-    if(delta < 0)
-    {
-        return -1.0;
-    }
-    else //Deux solutions, ou == 0 -> racine double
-    {
-        double tp = (-b + sqrt(delta)) / (2.0 * a);
-        double tm = (-b - sqrt(delta)) / (2.0 * a);
-
-        tp = fmax(tp, 0.0);
-        tm = fmax(tm, 0.0);
-
-        return fmin(tp, tm);
-    }
-}
 
 Ray move(Ray *r, double t)
 {
